8aeb900641e378bb32c229bb0087876d46d82477
Shital193 <shitalmodhavadiya193@gmail.com>
2025-03-27 21:26:33 +0530

    Updated changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Gemini-clone/',
+  base:'/MyGeminiWebsite/',
   plugins: [react()],
 })
+
